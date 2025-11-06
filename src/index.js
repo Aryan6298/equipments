f6d15be79c8ba2f6b54c8f118947398d174fc4b7
Aryan Janghela <ajrjaj123123@gmail.com>
2025-11-06 22:09:57 +0530

    cross-env used

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 // src/index.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-const basename =
-  process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : '/';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
